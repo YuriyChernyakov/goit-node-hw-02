f366cdb2e510c6b701ebf46ca170c7e4065a3f6a
Yuriy Chernyakov <nitro_06@ukr.net>
2023-06-13 13:06:39 +0300

    test

---
+++ controllers/users/register.js
@@ -0,0 +1,19 @@
+const { User } = require("../../models");
+const { Conflict } = require("http-errors");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`Email ${email} is already in use`);
+  }
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({ email, password: hashPassword, avatarURL });
+
+  res.status(201).json({ user: { email, subscription: newUser.subscription } });
+};
+
+module.exports = register;
---
+++ services/index.js
@@ -0,0 +1,13 @@
+const {
+    listContacts,
+    getContactById,
+    removeContact,
+    updateStatusContact,
+  } = require('./contactsService');
+  
+  module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    updateStatusContact,
+  };
